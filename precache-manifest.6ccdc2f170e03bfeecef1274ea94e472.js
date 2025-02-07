949a30fbf5336fa428626ad7f052cf672ec5b86c
loginmailgoogle <noticedactivitied@gmail.com>
2025-02-07 12:36:49 +0400

    Updates

---
+++ precache-manifest.6ccdc2f170e03bfeecef1274ea94e472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e97ee1fa0769be27a14d6deeb1ed3c",
+    "url": "/shiply/index.html"
+  },
+  {
+    "revision": "268d6f44618bc3815dd3",
+    "url": "/shiply/static/css/main.fafbb605.chunk.css"
+  },
+  {
+    "revision": "351706ade5f1b5d1c764",
+    "url": "/shiply/static/js/2.a6c45ac6.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/shiply/static/js/2.a6c45ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268d6f44618bc3815dd3",
+    "url": "/shiply/static/js/main.38661303.chunk.js"
+  },
+  {
+    "revision": "8de8ce9c2cd9b63965ea",
+    "url": "/shiply/static/js/runtime-main.9d488624.js"
+  }
+]);
